ae18b996a85e3f265f9c4b697295843f6eb97775
United-Ke <168593194+United-Ke@users.noreply.github.com>
2026-05-28 11:13:06 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -3,10 +3,10 @@
 // habari yako 😂 😂
 
 module.exports = {
-  BOT_NAME: process.env.BOT_NAME || 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ', 
-  OWNER_NAME: process.env.OWNER_NAME || 'Mr Lee', 
+  BOT_NAME: process.env.BOT_NAME || 'COMRADES CYBER', 
+  OWNER_NAME: process.env.OWNER_NAME || 'COMRADES CYBER', 
   OWNER_NUMBER: process.env.OWNER_NUMBER || '254116266407', 
-  SESSION_ID: process.env.SESSION_ID || '', // paste here your blinders session ID
+  SESSION_ID: process.env.SESSION_ID || 'blinder~3npmnBTD#4BgMlKyqlFCoLpt8IyaPlwq2ZTwKcPUVfa55V7KvE08', // paste here your blinders session ID
   MODE: process.env.MODE || "private", // public or private
   TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
 };
